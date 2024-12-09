97c4599d2e501316e7ef7bcbdb4873cfea8e7113
ErickG233 <1529896768@qq.com>
2024-12-09 18:31:06 +0800

    luci-app-qos-gargoyle: adapted for latest lede source

--- luci-app-qos-gargoyle/luasrc/model/qos_gargoyle.lua
+++ luci-app-qos-gargoyle/luasrc/model/qos_gargoyle.lua
@@ -3,30 +3,36 @@
 
 module("luci.model.qos_gargoyle", package.seeall)
 
-function has_ndpi()
-	return luci.sys.call("lsmod | cut -d ' ' -f1 | grep -q 'xt_ndpi'") == 0
-end
+-- 屏蔽ndpi模块
+-- function has_ndpi()
+-- 	return luci.sys.call("lsmod | cut -d ' ' -f1 | grep -q 'xt_ndpi'") == 0
+-- end
 
-function cbi_add_dpi_protocols(field)
-	local util = require "luci.util"
+-- 屏蔽ndpi模块
+-- function cbi_add_dpi_protocols(field)
+-- 	local util = require "luci.util"
 
-	local dpi_protocols = {}
+-- 	local dpi_protocols = {}
 
-	for line in util.execi("iptables -m ndpi --help 2>/dev/null | grep '^--'") do
-		local _, _, protocol, name = line:find("%-%-([^%s]+) Match for ([^%s]+)")
+-- 	for line in util.execi("iptables -m ndpi --help 2>/dev/null | grep '^--'") do
+-- 		local _, _, protocol, name = line:find("%-%-([^%s]+) Match for ([^%s]+)")
 
-		if protocol and name then
-			dpi_protocols[protocol] = name
-		end
-	end
+-- 		if protocol and name then
+-- 			dpi_protocols[protocol] = name
+-- 		end
+-- 	end
 
-	for p, n in util.kspairs(dpi_protocols) do
-		field:value(p, n)
-	end
-end
+-- 	for p, n in util.kspairs(dpi_protocols) do
+-- 		field:value(p, n)
+-- 	end
+-- end
 
 function get_wan()
        local net = require "luci.model.network".init()
-       local wan_nets = net:get_wan_networks()
-       return wan_nets and #wan_nets > 0 and wan_nets[1]
+       -- 保留参考代码
+	   -- local wan_nets = net:get_wan_networks()
+	   local wan_net = net:get_wannet()
+	   -- 保留参考代码
+       -- return wan_nets and #wan_nets > 0 and wan_nets[1]
+	   return wan_net
 end
